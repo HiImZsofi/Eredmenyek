274d64a082984c3e92c2c0e37ec9bf5dadf17bf9
HiImZsofi <zsofia.gyori00@gmail.com>
2022-10-20 10:43:14 +0200

    first commit

---
+++ src/hu/petrik/versenyeredmenyek/Eredemny.java
@@ -0,0 +1,35 @@
+package hu.petrik.versenyeredmenyek;
+
+public class Eredemny implements Comparable<Eredemny>{
+    private String reszIdo;
+    private String nev;
+
+    public Eredemny(String reszIdo, String nev) {
+        this.reszIdo = reszIdo;
+        this.nev = nev;
+    }
+
+    public String getReszIdo() {
+        return reszIdo;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+
+    @Override
+    public int compareTo(Eredemny other) {
+        String[] thisReszidok = this.reszIdo.split(":");
+        String[] otherReszidok = other.reszIdo.split(":");
+        Integer thisperc = Integer.parseInt(thisReszidok[0]);
+        Integer thismasodperc = Integer.parseInt(thisReszidok[1]);
+        Integer otherperc = Integer.parseInt(otherReszidok[0]);
+        Integer othermasodperc = Integer.parseInt(otherReszidok[1]);
+
+        if(thisperc.compareTo(otherperc) == 0){
+            return thismasodperc.compareTo(othermasodperc);
+        }
+        return thisperc.compareTo(otherperc);
+    }
+}
